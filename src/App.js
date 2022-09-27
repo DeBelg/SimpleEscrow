2aabd5408f954516319bb0827250f3569a8d8966
DeBelg <mff@foragers.io>
2022-09-27 18:51:33 +0200

    Added input fields and started mapping them for the initiater part

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Initiater from './components/Initiater';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+   
+        
+        <p> Do an online business deal using a trustless Middleman</p>
+    <Initiater/>
+        <p> You can't use our service yet. I have no clue how it works yet</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DeBelg/SimpleEscrow/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Created by Mf (DeBelg)
         </a>
       </header>
     </div>
